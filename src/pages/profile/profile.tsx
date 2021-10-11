972144035db5fd39f56c923eb6579af9f87e0ef7
Mohamed Rahoui <mrahoui@MacBook-Pro-de-Mohamed.local>
2021-10-11 23:40:38 +0200

    Profile WIP

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -7,7 +7,6 @@ import { Alert, Avatar, Button } from '@mui/material';
 import { useState } from 'react';
 import CopyTextToClipboard from '../../helpers/tools';
 import { Check } from '@mui/icons-material';
-import ShareArrow from '../../../assets/img/arrow-share.svg';
 
 const Profile = () => {
   const history = useHistory();
@@ -35,52 +34,47 @@ const Profile = () => {
     <div className={ST.container}>
       <div className={ST.heading}>Profil</div>
       <div className={ST.userCard}>
-        <div className={ST.content}>
-          <div className={ST.avatarWrapper}>
-            <Avatar
-              className={ST.avatar}
-              alt={getFullNameEmail(snap.user)}
-              src={snap.user?.image}
-            />
-          </div>
-          <div className={ST.infoWrapper}>
-            <div className={ST.info}>
-              <div className={ST.name}>{getFullNameEmail(snap.user)}</div>
-              {snap.user?.email !== getFullNameEmail(snap.user) &&
-                snap.user?.email && (
-                  <div className={ST.email}>{snap.user?.email}</div>
-                )}
-              <div className={ST.linkWrapper}>
-                <div className={ST.link}>
-                  <div className={ST.LinkText}>{`${
-                    import.meta.env.VITE_BASE_URL
-                  }/petition/${snap.user?.id}`}</div>
-                  <Button
-                    size='small'
-                    variant='contained'
-                    color={copied ? 'success' : 'primary'}
-                    onClick={() => copy()}
-                    endIcon={copied ? <Check /> : null}
-                    className={ST.copy}
-                  >
-                    {copied ? 'Copié' : 'Copier'}
-                  </Button>
-                </div>
-                <img src={ShareArrow} className={ST.shareArrow} />
-              </div>
-              <div></div>
-            </div>
-          </div>
-          <div className={ST.ShareText}>
-            Partagez ce lien avec vos amis et gagnez des points pour chaque
-            signature validée.
-          </div>
+        <Avatar
+          className={ST.avatar}
+          alt={getFullNameEmail(snap.user)}
+          src={snap.user?.image}
+        />
+        <div className={ST.info}>
+          <div className={ST.name}>{getFullNameEmail(snap.user)}</div>
+          {snap.user?.email !== getFullNameEmail(snap.user) &&
+            snap.user?.email && (
+              <div className={ST.email}>{snap.user?.email}</div>
+            )}
         </div>
+        <div className={ST.divider}></div>
+        <div className={ST.scores}>
+          <div className={ST.score}>Points disponibles : 5</div>
+          <div className={ST.score}>Signatures validées : 30</div>
+          <div className={ST.score}>Signatures en cours : 9</div>
+        </div>
+        <div className={ST.ShareText}>
+          Partagez ce lien avec vos amis et gagnez des points pour chaque
+          signature validée.
+        </div>
+        <div className={ST.link}>
+          <div className={ST.LinkText}>{`${
+            import.meta.env.VITE_BASE_URL
+          }/petition/${snap.user?.id}`}</div>
+          <Button
+            size='small'
+            variant='contained'
+            color={copied ? 'success' : 'primary'}
+            onClick={() => copy()}
+            endIcon={copied ? <Check /> : null}
+            className={ST.copy}
+          >
+            {copied ? 'Copié' : 'Copier'}
+          </Button>
+        </div>
+        <Button variant='contained' onClick={() => logout()}>
+          Déconnexion
+        </Button>
       </div>
-
-      <Button variant='contained' onClick={() => logout()}>
-        Déconnexion
-      </Button>
     </div>
   );
 };
