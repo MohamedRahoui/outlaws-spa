924e9c52761466a04e71689e7a801d529bd609c7
Mohamed Rahoui <mrahoui@MacBook-Pro-de-Mohamed.local>
2021-10-25 03:56:49 +0200

    Sentry

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,6 +2,7 @@ interface ImportMetaEnv extends Readonly<Record<any, any>> {
   readonly VITE_APP_TITLE: string;
   readonly VITE_GOOGLE_CLIENT_ID: string;
   readonly VITE_RECAPTCHA_KEY: string;
+  readonly VITE_SENTRY: string;
 }
 
 interface ImportMeta {
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,19 @@ import App from './App';
 import './theme/main.scss';
 import { Router } from 'react-router-dom';
 import history from './helpers/history';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY,
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0,
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <GoogleReCaptchaProvider reCaptchaKey={import.meta.env.VITE_RECAPTCHA_KEY}>
